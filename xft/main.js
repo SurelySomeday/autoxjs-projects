b46e973902440a72bb9b7a38c5311573f07c75f7
yanxin <648951430@qq.com>
2024-07-12 13:41:47 +0800

    可以发送邮件

--- xft/main.js
+++ xft/main.js
@@ -18,73 +18,121 @@ function getScreenCapture() {
         return true;
     }
 }
-log("脚本开始");
-//确定无障碍已经打开
-auto.waitFor();
-log("打开xft");
-launch("com.cmbchina.xft");
-sleep(1000);
-//允许打开app
-//let Allow = textMatches(/(允许|立即开始|统一)/).findOne(2 * 1000);
-//if (Allow) {
-//	Allow.click();
-//}
 
-log("连续点击两次返回，确保在主页");
-// 连续点击两次返回，确保在主页
-sleep(3000);
-click(28, 128);
-sleep(1000);
-click(28, 128);
-
-// -- 这时候xft是已经打开的
-// -- 下一步确认打卡按钮已经加载完毕
-//循环找色，找到蓝色(#3285ff)时停止并报告坐标
-log("获取截图权限，准备截图");
-getScreenCapture();
-requestScreenCapture();
-while(true){
-    var img = captureScreen();
-    var point = findColor(img, "#3285ff", {
-        region: [202, 952, 1,1],
-        threshold: 4
-        });
-    if(point){
-        toast("找到打卡模块按钮，坐标为(" + point.x + ", " + point.y + ")");
-        break;
-    }
+//打开app
+function openApp(str) {
+    log("脚本开始");
+    //确定无障碍已经打开
+    auto.waitFor();
+    launch(str);
+    //允许打开app
+    //let Allow = textMatches(/(允许|立即开始|统一)/).findOne(2 * 1000);
+    //if (Allow) {
+    //	Allow.click();
+    //}
 }
 
-
-// -- 这时候打卡按钮已经出现
-
-//点击打卡
-click(200, 1000);
-sleep(5000)
-//循环找色，找到蓝色(#3e76ff)时停止并报告坐标
-while(true){
-    var img = captureScreen();
-    var point = findColor(img, "#3e76ff", {
-        region: [660, 1719, 200, 200],
-        threshold: 4
-        });
-    if(point){
-        toast("找到签到按钮，坐标为(" + point.x + ", " + point.y + ")");
-        break;
+//邮件推送打卡结果
+function sendResult(img, msg) {
+    sleep(1000);
+    app.sendEmail({
+        email: ["648951430@qq.com"],
+        subject: "打卡结果",
+        text: msg
+    });
+    var button = text("电子邮件").findOne()
+    if(button){
+        toast("默认选择电子邮件");
+        button.parent().click();
     }
+
+    sleep(1000);
+    id("to").findOne().setText("648951430@qq.com");
+    id("do_attach").findOne().click();
+    var button = text("相册").findOne()
+    if(button){
+        button.parent().click();
+    }
+    id("grid").findOne().children().forEach(child => {
+        var target = child.findOne(id("pick_num_indicator"));
+        if(target != null){
+            target.parent().click();
+        }
+    });
+    sleep(1000);
+    id("compose_send_btn").findOne().click();
+
 }
-log("点击签到按钮");
-//确认打卡
-click(550, 1750)
-sleep(3000)
-//确认打卡弹窗
-log("点击打卡后的知道了");
-click(558,1590)
-sleep(3000)
-// 结束打卡，返回上一级
-log("结束打卡返回上一级");
-click(51,150)
-sleep(500)
-log("结束打卡返回home");
-home()
-log("脚本结束");
+try{
+    log("脚本开始");
+    log("打开xft");
+    openApp("com.cmbchina.xft")
+    sleep(1000);
+    log("连续点击两次返回，确保在主页");
+    // 连续点击两次返回，确保在主页
+    sleep(3000);
+    click(28, 128);
+    sleep(1000);
+    click(28, 128);
+
+    // -- 这时候xft是已经打开的
+    // -- 下一步确认打卡按钮已经加载完毕
+    //循环找色，找到蓝色(#3285ff)时停止并报告坐标
+    log("获取截图权限，准备截图");
+    getScreenCapture();
+    requestScreenCapture();
+    while(true){
+        var img = captureScreen();
+        var point = findColor(img, "#3285ff", {
+            region: [202, 952, 1,1],
+            threshold: 4
+            });
+        if(point){
+            toast("找到打卡模块按钮，坐标为(" + point.x + ", " + point.y + ")");
+            break;
+        }
+    }
+
+
+    // -- 这时候打卡按钮已经出现
+
+    //点击打卡
+    click(200, 1000);
+    sleep(5000)
+    //循环找色，找到蓝色(#3e76ff)时停止并报告坐标
+    while(true){
+        var img = captureScreen();
+        var point = findColor(img, "#3e76ff", {
+            region: [660, 1719, 200, 200],
+            threshold: 4
+            });
+        if(point){
+            toast("找到签到按钮，坐标为(" + point.x + ", " + point.y + ")");
+            break;
+        }
+    }
+    log("点击签到按钮");
+    //确认打卡
+    click(550, 1750)
+    sleep(3000)
+    //className("android.view.View").text("签退").findOne().click()
+    //确认打卡弹窗
+    log("点击打卡后的知道了");
+    click(558,1590)
+    sleep(3000)
+    var img = captureScreen("/sdcard/res.png");
+    sendResult(img, "打卡成功");
+    sleep(1000)
+    // 结束打卡，返回上一级
+    log("结束打卡返回上一级");
+    click(51,150)
+    sleep(500)
+    log("结束打卡返回home");
+    home()
+    log("脚本结束");
+}catch(error){
+    // 处理异常
+    log("An exception occurred: ", error);
+    var img = captureScreen("/sdcard/res.png");
+    sendResult(img, "打卡失败,异常原因：" + error);
+}
